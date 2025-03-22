1c1e81969f3e3c9feb3e4af31a87edd7a51a5f7d
Anara <berdali.aygerim9@gmail.com>
2025-03-22 14:18:23 +0400

    React vite setup, packges added, Sidebar and Homepage added

---
+++ src/pages/Favoritepage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Favoritepage extends Component {
+  render() {
+    return (
+      <div>Favoritepage</div>
+    )
+  }
+}
+
+export default Favoritepage
